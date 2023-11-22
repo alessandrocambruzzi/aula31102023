3ac6832e342866fd5332ecf38564ab8b096c3bd8
alessandrocambruzzi <146895401+alessandrocambruzzi@users.noreply.github.com>
2023-11-22 01:29:10 +0000

    fix: adicionado exclusão de curso

--- app/admin/course/page.tsx
+++ app/admin/course/page.tsx
@@ -1,12 +1,19 @@
 import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
 
 export const revalidate =0
 
 export default async function ListCourse() {
+    async function deleteCourse(formData: FormData){
+        "use server"
+        const id = formData.get("id") as string;
+        await sql`DELETE from courses where id=${id}`
+        revalidatePath("/admin/course")
+    }
     const { rows } = await sql`SELECT * from courses`;
     return (
         <div>
-            <h1 className="text-center text-white">Lista de Curso</h1>
+            <h1 className="text-center text-white">Lista de Cursos</h1>
 
             <table>
                 <thead>
@@ -16,7 +23,15 @@ export default async function ListCourse() {
                     {
                         rows.map((course) => {
                             return (
-                                <tr key={course.id}><td>{course.title}</td> <td>{course.description}</td> </tr>
+                                <tr key={course.id}><td>{course.title}</td> <td>{course.description}</td> 
+                                <td>
+                                    <form >
+                                     <input type="text" hidden name="id" value={course.id}/>   
+                                    <button formAction={deleteCourse}>Excluir</button>
+                                    </form>
+                                
+                                </td> 
+                                </tr>
                             )
                         })
                     }
