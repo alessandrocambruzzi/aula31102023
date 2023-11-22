646681168d12aa990ee96faff3ea5849f718ac0a
alessandrocambruzzi <146895401+alessandrocambruzzi@users.noreply.github.com>
2023-11-22 01:01:49 +0000

    fix: adicionado botão uploud

--- app/admin/course/new/page.tsx
+++ app/admin/course/new/page.tsx
@@ -1,23 +1,37 @@
+import UploadButton from "@/app/components/UploadButton";
 import { sql } from "@vercel/postgres";
-export default async function New(){
-    async function saveCourse (formData: FormData){
+import { useSearchParams } from "next/navigation";
+
+export const revalidate =0
+
+export default function NewCourse({
+    searchParams,
+  }: {
+    searchParams?: {
+      url?: string;  
+    };
+  }){
+    
+    const urlImage = searchParams?.url || '';
+
+    async function saveCourse(formData: FormData){
         "use server"
         const title = formData.get("title") as string;
         const description = formData.get("description") as string;
-        const url = formData.get ("URL")as string;
-        await sql `INSERT INTO courses (title,description,url) VALUES ( ${title},${description},${url} )`
+        await sql`INSERT INTO courses (title, description,url) VALUES(${title}, ${description}, ${urlImage})`
         console.log("Acessou a função")
     }
     return (
         <div>
             <h1 className="text-white text-center text-4xl">Cadastrar Cursos</h1>
             <form>
-                <input type="text" name="title" placeholder="Digite o Titulo do Curso"/><br/>
-                <input type="text" name="description" placeholder="Digite a descrição do Curso"/><br/>
-                <input type="text" name="URL" placeholder="Digite a URL da imagem"/><br/>
+                <input type="text" name="title" placeholder="Digite o Título do Curso"/><br/><br/>
+                <input type="text" name="description" placeholder="Digite a Descriçao do curso"/> <br/><br/>
+                <br/>
+                <UploadButton />
                 <button formAction={saveCourse} className="text-white">Salvar</button>
-
             </form>
         </div>
+
     )
 }
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 export default function Admin(){
     return (
         <div>
-        <h1 className="text-center text-xl"> O ADM ESTÁ ONLINE</h1>
+        <h1 className="text-center text-xl">Area ADM</h1>
         <a href="/admin/course"> Listar Cursos </a>
         <a href="/admin/course/new"> Cadastrar Curso </a>
         </div>
