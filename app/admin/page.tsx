de188ab6d719596b195e6243dbb20fca9d37353a
alessandrocambruzzi <146895401+alessandrocambruzzi@users.noreply.github.com>
2023-11-29 00:36:41 +0000

    fix: vendo como ta

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,12 @@
 export default function Admin(){
     return (
         <div>
-        <h1 className="text-center text-xl">Area ADM</h1>
-        <a href="/admin/course"> Listar Cursos </a>
-        <a href="/admin/course/new"> Cadastrar Curso </a>
+        <h1 className="text-center text-xl">Seja Bem Vindo</h1>
+        
+      
+    
+
         </div>
+        
     )
 }
