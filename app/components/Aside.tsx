89de51046875bddf6e27973064e508bdd419316b
alessandrocambruzzi <146895401+alessandrocambruzzi@users.noreply.github.com>
2023-11-29 00:49:07 +0000

    fix: aside tsx add

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -1,10 +1,9 @@
-export default function NavBar(){
-    return(
-        <div className="p-12 flex justify-between items-center">
-            <h1>Logo</h1>
-            <h1>Navegação</h1>
-            <h1>Nome Usuario</h1>
-        </div>
+export default function Aside() {
+    return (
+        <div className="w-1/6">
+            <a href="/admin/course">Listar Curso</a> <br/>
+            <a href="/admin/course/new">Cadastrar Curso</a>
 
+        </div>
     )
 }
